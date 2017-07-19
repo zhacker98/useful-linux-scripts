2fa9be3e65b339190e9339de19139418ec452cd9
joelewhite <joelewhite@debin-ws-j>
2017-07-19 17:05:03 -0400

    Expanding the ansible_create_playbook.sh script

--- configuration_mgmt/ansible/ansible_create_playbook.sh
+++ configuration_mgmt/ansible/ansible_create_playbook.sh
@@ -1,25 +1,35 @@
 #! /bin/bash
 
-# A Script to generate the directory structure for new playbooks
+# A Script to create Ansible Playbook Structure
+#       Per Ansible Best Practices
 #
-#	Written by Joel E White
-#
-# Directory structure based off of
-# http://docs.ansible.com/ansible/playbooks_best_practices.html
+#       Written by Joel E White
+#         2017
 
+# Variable definitions
 working_dir="/etc/ansible/roles/"
 new_playbook=$1
 
+# Preliminary
+if [[ -z ${new_playbook} ]]; then
+  echo "No ARG1 provided, this represents the playbook name"
+  exit
+fi
+
+exists_playbook=$(find ${working_dir} -type d -name "${new_playbook}")
+if [[ -z "${exists_playbook}" ]]; then
+  echo "Proceeding"
+else
+  echo "A playbook with the name ${new_playbook} already exists in the target directory"
+fi
+
+# Main
 cd ${working_dir}
 
 mkdir -p ${working_dir}/${new_playbook}
 
 # create playbook directories
-for i in tasks handlers templates files vars defaults meta library lookup_plugins
-  do mkdir -p ${working_dir}/${new_playbook}/${i}
-done
+for i in tasks handlers templates files vars defaults meta library lookup_plugins; do mkdir -p ${working_dir}/${new_playbook}/${i}; done
 
 # create playbook main.yml files
-for i in tasks handlers vars meta
-  do touch ${working_dir}/${new_playbook}/${i}/main.yml
-done
+for i in tasks handlers vars meta; do touch ${working_dir}/${new_playbook}/${i}/main.yml; done
