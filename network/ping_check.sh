d70de02f5b4fd340bf7cd192547d89d7e80dcd64
zhacker98 <zhacker98@users.noreply.github.com>
2017-08-29 15:35:45 -0400

    Create ping_check.sh

---
+++ network/ping_check.sh
@@ -0,0 +1,31 @@
+
+#! /bin/bash
+
+#####
+# -q quiet
+# -c nb of pings to perform
+#
+# May need to CTRL-Z to break the loop
+#
+# Written by Joel E White
+
+# VARIABLES
+TIMESTAMP="$(date +%m-%d-%Y::%H:%M:%S)"
+BASE_DIR="$(pwd)"
+LOG_DIR="${BASE_DIR}/LOGS"
+LOG_FILE="${LOG_DIR}/SENTRY_CONNECTION_LOG"
+
+CHECK_IP="xxx.xxx.xx.xx"
+
+# PRELIMINARY
+mkdir -p ${LOG_DIR}
+touch ${LOG_FILE}
+
+# MAIN
+while true; do
+  ping -q -c1 ${CHECK_IP} > /dev/null
+  if [[ $? -ne 0 ]]; then
+    echo "${TIMESTAMP} -- CONNECTION TO SENTRY IS DOWN" >> ${LOG_FILE}
+  fi
+  TIMESTAMP="$(date +%m-%d-%Y::%H:%M:%S)"
+done
