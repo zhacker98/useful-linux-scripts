24c1eb21632d3b1d2dacd5296a01d5a8eb5505c6
zhacker98 <zhacker98@users.noreply.github.com>
2018-01-29 13:18:15 -0500

    Create auth_check.sh

---
+++ auth_check.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+#####
+#
+#   A Script to check if you have auth on a list of servers
+#
+#   Written by Joel E White
+#
+#####
+
+# VAR
+BaseDir=$(pwd)
+LogDir="${BaseDir}/auth_check-logs"
+LogFile="${LogDir}/auth_check-log_$(date +%Y-%m-%d_%H:%M)"
+
+SshOption1="-oConnectTimeout=5"
+SshOption2="-oStrictHostKeyChecking=no"
+SshOption3="-oPasswordAuthentication=no"
+
+CommandsToRun="whoami; hostname -f"
+
+ServerList="$(cat /dir/to/server.list)"
+
+
+# PRE
+
+mkdir -p ${LogDir}
+touch ${LogFile}
+
+# MAIN
+for hOst in ${ServerList}
+  do echo ">>---- ${hOst} ----<<" | tee -a ${LogFile}
+  ssh -o ${ShhOption1} -o ${SshOption2} -o ${SshOption3} ${hOst} ${CommandsToRun} | tee -a ${LogFile}
+  echo '<<---- ** ---->>' | tee -a ${LogFile}
+done
