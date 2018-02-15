57eac70c179f3cffacc8e1c5605bf0608249cf7e
zhacker98 <zhacker98@users.noreply.github.com>
2018-02-15 11:17:19 -0500

    Create netcat_email.sh
    Work in progress, have not tested out the logic yet

---
+++ netcat_email.sh
@@ -0,0 +1,43 @@
+# DERIVED FROM https://www.linuxjournal.com/content/sending-email-netcat
+#! /bin/bash
+
+#####
+#
+#   A Script to send an email via netcat to a given smtp server
+#
+#####
+
+# VAR
+NC=$(which nc)
+Date=$(date '+%a, %d %b %Y %H:%M:%S %z')
+From=$1
+To=$2
+Subject=$3
+Body=$4
+
+SMTPserver='smtp.example.com'
+SMTPport='25'
+
+Message="
+HELO ${SMTPserver}
+   MAIL FROM:<test@host.example.com>
+   RCPT TO:<${To}>
+   DATA
+   From: [Alice] <${From}>
+   To: <${To}>
+   Date: ${Date}
+   Subject: ${Subject}
+
+   ${Body}
+
+
+   .
+   QUIT
+   "
+
+# PRE
+
+
+# MAIN
+
+${NC} ${SMTPserver} ${SMTPport} < ${Message}
