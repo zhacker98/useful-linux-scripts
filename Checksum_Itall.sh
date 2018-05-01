b1956cff1b1e7d888328b352179a3c84adc416ea
zhacker98 <zhacker98@users.noreply.github.com>
2018-05-01 09:36:26 -0400

    Create Checksum_Itall.sh

---
+++ Checksum_Itall.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# VAR
+SCAN_DIR="$1"
+EXTEN="$2"
+
+TMP_COUNTER="$(pwd)/tmp.counter"
+
+Usage="./script <directory_to_scan> <extension>"
+
+# PRE
+if [ $(ls ${TMP_COUNTER}|wc -l) -ne "0" ]; then
+  rm ${TMP_COUNTER}  
+fi
+
+touch ${TMP_COUNTER}
+
+
+for i in $(ls ${SCAN_DIR}/*.${EXTEN}); do ls ${i}|grep -v 'sha256' >> ${TMP_COUNTER}; done
+
+for i in $(cat ${TMP_COUNTER}|grep -v 'tmp.counter'); do sha256sum ${i} > ${i}.sha256; done
+
+
+# MAIN
+
+#for i in $(ls /root/Cron/*.sh); do sha256sum ${i} > ${i}.sha256; done
+
+cd ${SCAN_DIR}; for i in $(ls ${SCAN_DIR}/*.sha256); do cat ${i}|sha256sum -c; done
+
+rm ${TMP_COUNTER}
